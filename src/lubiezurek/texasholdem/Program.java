7d23dd18b752901392c01d1a67615538199a5d79
MrMino <assassinking@gmail.com>
2015-12-04 23:38:32 +0100

    Dodana obsługa domyślnych argumentów Dodane domyślne argumenty: port dla serwera (-s), host i port dla klienta (-c)

--- src/lubiezurek/texasholdem/Program.java
+++ src/lubiezurek/texasholdem/Program.java
@@ -7,29 +7,39 @@ import lubiezurek.texasholdem.server.Server;
 
 public class Program {
 	static final String DEFAULT_PORT = "7777";
+	static final String DEFAULT_LOCALHOST = "127.0.0.1";
 
 	public static void main(String[] args) {
 		try {
-			if(args.length < 1) {
+			if(args.length == 0) {
 				Logger.error("Invalid number of arguments");
 				return;
 			}
 			
 			String typ = args[0];
-			if(typ.equals("-s") || typ.equals("--server")) {
-				if(args.length != 2) {
-					args = new String[2];
-					args[0] = "-s";
-					args[1] = DEFAULT_PORT; 
+
+			//Wstawianie domyślnych argumentów, jeśli brak innych
+			if(args.length == 1) {
+				if(typ.equals("-s") || typ.equals("--server")){
+					String[] default_args = new String[3];
+					default_args[0] = args[0];
+					default_args[1] = DEFAULT_PORT;
+					args = default_args;
 				}
+				else if(typ.equals("-c") || typ.equals("--client")){
+					String[] default_args = new String[3];
+					default_args[0] = args[0];
+					default_args[1] = DEFAULT_LOCALHOST;
+					default_args[2] = DEFAULT_PORT;
+					args = default_args;
+				}
+			}
+			
+			if(typ.equals("-s") || typ.equals("--server")) {
 				Server server = new Server(args);
 				server.run();
 			}
 			else if(typ.equals("-c") || typ.equals("--client")) {
-				if(args.length != 3) {
-					Logger.error("Invalid number of arguments");
-					return;
-				}
 				Client client = new Client(args);
 				client.run();
 			}
