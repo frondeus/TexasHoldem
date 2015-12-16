654505b8e910e7c47d6dbdc9a4f87c3af10e01c2
Wojciech Polak <frondeus@gmail.com>
2015-12-16 10:00:48 +0100

    Create SimpleTest.java
    Na szybko sprawdzanie czy unit testy działają

---
+++ src/lubiezurek/texasholdem/tests/SimpleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class SimpleTest {
+  @Test
+  public void simpleTest() {
+    int a = 0;
+    int b = 1-1;
+    assertEquals("b must be 0", a, b);
+  }
+}
