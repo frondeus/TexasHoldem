c944463308a63f009716833fef2b89bb5185119f
Frondeus <frondeus@gmail.com>
2016-01-17 22:23:01 +0100

    Więcej animacji

--- websocket/app/main.js
+++ websocket/app/main.js
@@ -1,40 +1,56 @@
-define(["jquery", "domReady!", "./logger", "./player", "./socket", "./string"], function($ , doc, logger, player, socket ) {
-    var myUUID = null;
+define(["jquery", "domReady!", "./logger", "./player", "./socket", "./string", "./random", "./card"],
+       function($ , doc, logger, player, socket, str, random, card ) {
 
-    if(!socket) return null;
+           var myUUID = null;
 
-    logger.log("Connecting...");
+           if(!socket) return null;
 
-    var onChat = function(event) {
+           var onChat = function(event) {
 
-        var playerUUID = event.arguments.shift();
-        var currentPlayer = player.getPlayer(playerUUID);
-        logger.log(currentPlayer.color.capitalizeFirstLetter() + " said: " + event.arguments.join(" "), currentPlayer.color);
-    };
+               var playerUUID = event.arguments.shift();
+               var currentPlayer = player.getPlayer(playerUUID);
+               logger.log(currentPlayer.color.capitalizeFirstLetter() + " said: " + event.arguments.join(" "), currentPlayer.color);
+           };
 
-    var onClientConnect = function(event) {
+           var onClientConnect = function(event) {
 
-        var playerUUID = event.arguments.shift();
-        var currentPlayer = player.getPlayer(playerUUID);
-        logger.log(currentPlayer.color.capitalizeFirstLetter() + " joined to the table");
-    };
+               var playerUUID = event.arguments.shift();
+               var currentPlayer = player.getPlayer(playerUUID);
+               logger.log(currentPlayer.color.capitalizeFirstLetter() + " joined to the table");
+           };
 
-    socket.setHandlers({
-        onChat: onChat,
-        onClientConnect: onClientConnect
-    });
+           socket.setHandlers({
+               onChat: onChat,
+               onClientConnect: onClientConnect
+           });
 
-    $("#form").submit(function(event){
-        var value = $("#text-input").val();
-        var message = {
-            command: "chat",
-            arguments: value.split(" ")
-        };
+           $(".deck").click(function(event) {
+               $(this).toggleClass("hover");
+               if($(this).hasClass("hover")) {
+                   card.random($(".front", this));
 
-        socket.send(JSON.stringify(message));
+                   var firstBlank = $(".table .card.blank")[0];
+                   if(firstBlank) {
+                       var firstSlot = $(firstBlank).removeClass("blank").parent();
+                       firstSlot.addClass("hover");
 
-        $("#text-input").val("");
-        $("#log").animate({ scrollTop: $("#log")[0].scrollHeight}, 1000);
-        event.preventDefault();
-    });
-});
+                       var firstFront = $(".front", firstSlot);
+                       card.random(firstFront);
+                   }
+               }
+           });
+
+           $("#form").submit(function(event){
+               var value = $("#text-input").val();
+               var message = {
+                   command: "chat",
+                   arguments: value.split(" ")
+               };
+
+               socket.send(JSON.stringify(message));
+
+               $("#text-input").val("");
+               $("#log").animate({ scrollTop: $("#log")[0].scrollHeight}, 1000);
+               event.preventDefault();
+           });
+       });
